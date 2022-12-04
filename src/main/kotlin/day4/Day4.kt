1f0cdbde41e3be1fe96328f1e42064fe7c01fc8e
Michael Rudolph <michael@rudolph.io>
2022-12-04 18:20:09 +0100

    Day 4

---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,32 @@
+package day4
+
+import base.FileHandling
+import base.expect
+import base.result
+
+fun main() {
+    Day4()
+}
+
+class Day4 {
+
+    init {
+        getInput("test.txt").resolveFirst().expect(2)
+        getInput("input.txt").resolveFirst()
+        getInput("test.txt").resolveSecond().expect(4)
+        getInput("input.txt").resolveSecond()
+    }
+
+    private fun List<List<Int>>.resolveFirst(): Int = count { ranges ->
+        (ranges[0] >= ranges[2] && ranges[1] <= ranges[3])
+                || (ranges[2] >= ranges[0] && ranges[3] <= ranges[1])
+    }.result()
+
+    private fun List<List<Int>>.resolveSecond() = count { ranges ->
+        (ranges[0]..ranges[1]).intersect(ranges[2]..ranges[3]).isNotEmpty()
+    }.result()
+
+    private fun getInput(filename: String) = FileHandling("/day4/$filename").map { line ->
+        line.split(",", "-").map(String::toInt)
+    }
+}
